3dafba3fc25be9270932ebed06b0fcee116d172e
James Blackledge <james.blackledge@ada.ac.uk>
2018-07-18 15:52:01 +0100

    Probably Broke Something!

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,7 +4,7 @@ import Styles from "./App.less";
 
 import Scheduler from '../../components/Scheduler/Scheduler';
 
-class App extends Component {
+export default class App extends Component {
 
     componentDidMount() {
         console.log('ComponentDidMount');
@@ -15,66 +15,7 @@ class App extends Component {
 
     state = {
         record: null,
-        days: [
-            {
-                number: '21',
-                slots: [
-                    {
-                        startTime: '9am',
-                        endTime: '11am'
-                    },
-                    {
-                        startTime: '12pm',
-                        endTime: '2pm'
-                    }
-                ]
-            },
-            {
-                number: '23',
-                slots: [
-                    {
-                        startTime: '9am',
-                        endTime: '10am'
-                    }
-                ]
-            }
-        ],
         selectedDates: []
-    }
-
-    newSessionHandler = () => {
-        const day = {...this.state.days[1]};
-        const newSlots = [...day.slots];
-        newSlots.push({
-            startTime: '11am',
-            endTime: '2pm'
-        });
-        day.slots = newSlots;
-
-        const newDays = [...this.state.days];
-        newDays[1] = day;
-        
-        this.setState({
-            days: newDays
-        });
-    }
-
-    deleteDayHandler = (dayNumber) => {
-        const days = this.state.days;
-        // const updatedDays = days.filter((day) => {
-        //     return day.number != dayNumber;
-        //     // if (day.number == dayNumber) {
-        //     //     return false;
-        //     // } else {
-        //     //     return true;
-        //     // }
-        // });
-
-        const updatedDays = days.filter(day => day.number != dayNumber);
-
-        this.setState({
-            days: updatedDays
-        });
     }
 
     setDateHandler = (newDate) => {
@@ -85,6 +26,8 @@ class App extends Component {
 
         if (currentDates.indexOf(newDate) == -1 && newDate >= startOfToday.valueOf()) {
             currentDates.push(newDate);
+
+            this.props.record.set('createdAt', newDate.valueOf());
 
             this.setState({
                 selectedDates: currentDates
@@ -109,12 +52,9 @@ class App extends Component {
     }
 
     render() {
-        console.log('Render');
-
-        const record = this.props.record;
-        // DOESN'T WORK: record.createdAt
-        // const createdAt = record.get('createdAt');
-        // console.log(createdAt);
+        // DOESN'T WORK: this.props.record.createdAt
+        console.log(moment(this.props.record.get('createdAt')).format('L'));
+        console.log(this.props.record);
 
         const selectedDates = this.state.selectedDates.sort().map(date => {
             const momentDate = moment(date);
@@ -123,12 +63,10 @@ class App extends Component {
         });
 
         return <div>
-            <Scheduler days={this.state.days} onDeleteDay={this.deleteDayHandler} />
-            <button onClick={this.newSessionHandler}>Add New Session</button>
-
             <quip.apps.ui.CalendarPicker
                 initialSelectedDateMs={Date.now()}
-                onChangeSelectedDateMs={this.setDateHandler}/>
+                onChangeSelectedDateMs={this.setDateHandler}
+            />
 
             <p>Selected dates:</p>
             <ul>
@@ -137,5 +75,3 @@ class App extends Component {
         </div>;
     }
 }
-
-export default App;
