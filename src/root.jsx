bedde77f01cf0f6f26dc38f732d59b18ec03788e
Niccolò Zapponi <nzapponi@salesforce.com>
2018-07-20 07:56:06 +0100

    Added support for user locale

--- src/root.jsx
+++ src/root.jsx
@@ -1,4 +1,5 @@
 import quip from "quip";
+import moment from 'moment';
 import App from "./containers/App/App";
 
 import { Root, Timeslot, Response } from './model';
@@ -10,6 +11,9 @@ quip.apps.registerClass(Response, 'response');
 quip.apps.initialize({
     initializationCallback: function(rootNode) {
         const rootRecord = quip.apps.getRootRecord();
+    
+        const userLocale = window.navigator.userLanguage || window.navigator.language;
+        moment.locale(userLocale);
 
         ReactDOM.render(<App record={rootRecord} />, rootNode);
     },
