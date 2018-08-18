46c9a426301244988ea83ee6b96fad4a95b204ec
Niccolò Zapponi <nzapponi@salesforce.com>
2018-08-18 15:31:49 +0100

    Fixed bug with small screens

--- src/components/Scheduler/Scheduler.js
+++ src/components/Scheduler/Scheduler.js
@@ -8,7 +8,7 @@ const scheduler = (props) => {
     const days = props.days;
     const styles = [Styles.SchedulerBox];
 
-    if (props.containerWidth < 800) {
+    if (quip.apps.isMobile()) {
         styles.push(Styles.Mobile);
     }
     
@@ -24,10 +24,10 @@ const scheduler = (props) => {
             createTimeslot={props.createTimeslot}
             deleteTimeslot={props.deleteTimeslot}
             deleteDate={props.deleteDate}
-            isMobile={props.containerWidth < 800} />);
+            isMobile={quip.apps.isMobile()} />);
     }
 
-    return <div className={styles.join(' ')} style={{width: props.containerWidth < 800 ? props.containerWidth : null}}>
+    return <div className={styles.join(' ')} style={{width: quip.apps.isMobile() ? props.containerWidth : null}}>
         {dayComponents}
     </div>;
 };
